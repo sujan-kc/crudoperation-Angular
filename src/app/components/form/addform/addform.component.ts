044a1751053e9118575889b91c7526ea2f4215e6
sujan-kc <98309411+sujan-kc@users.noreply.github.com>
2022-08-04 13:18:07 +0545

    x

--- src/app/components/form/addform/addform.component.ts
+++ src/app/components/form/addform/addform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormService } from 'src/app/service/form.service';
 import { ActivatedRoute } from '@angular/router';
@@ -22,34 +22,59 @@ export class AddformComponent implements OnInit {
       this.fs.getDataById(this.id).subscribe(res => {
         console.log(res)
         this.createForm(res)
-
-
-
-
-
       })
     }
     this.createForm()
   }
 
   createForm(item: any = {}) {
-
-    console.log("createform", item)
     this.form = this.fb.group({
       // id: new FormControl(item.id),
       // name: new FormControl(item.name),
       // class: new FormControl(item.class),
       // address: new FormControl(item.address)
-
-
       //next method for form control 
       id: [item.id],
-      name: [item.name ? item.name : 'avisek'],
+      name: [item.name ? item.name : ''],
       class: [item.class],
-      address: [item.address]
-
+      address: [item.address],
+      result: this.fb.array(
+        (
+          () => {
+            // if not of item.result => return  []
+            if (!item.result) {
+              return [this.createStudentDetails()]
+            }
+            // 
+            else {
+              return item.result.map((res: any) => this.createStudentDetails(res))
+            }
+          })()
+      )
     })
   }
+
+  createStudentDetails(item: any = {}) {
+    return this.fb.group({
+      id: [item.id ? item.id : 0],
+      subject: [item.subject ? item.subject : ''],
+      marksObtained: [item.marksObtained ? item.marksObtained : null]
+    })
+  }
+
+  get studentDetails() {
+    return this.form.get('result') as FormArray
+    // result => resutl= [ ]
+  }
+
+  addFormArrGroup() {
+    this.studentDetails.push(this.createStudentDetails())
+  }
+
+  // get() {
+  //   console.log(this.studentDetails)
+  //   console.log(this.studentDetails.controls[0].get('subject')?.value)
+  // }
   postData() {  //data aayo vane yo lline of code 
     if (this.id) {
       this.fs.updateData(this.form.value, this.id).subscribe(() => {
